2a9c76777a4718979b13454635f6a699c1a0da89
tithigoswami <tithigoswami2004@gmail.com>
2025-01-23 09:29:43 +0530

    Create setdatatype.py

---
+++ setdatatype.py
@@ -0,0 +1,8 @@
+# initializing empty set
+s1 = set()
+
+s1 = set("GeeksForGeeks")
+print("Set with the use of String: ", s1)
+
+s2 = set(["Geeks", "For", "Geeks"])
+print("Set with the use of List: ", s2)
