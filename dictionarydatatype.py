236a981eaf94fb346a69cb16fd5e048d06860b8d
tithigoswami <tithigoswami2004@gmail.com>
2025-01-23 09:30:43 +0530

    Create dictionarydatatype.py

---
+++ dictionarydatatype.py
@@ -0,0 +1,10 @@
+# initialize empty dictionary
+d = {}
+
+
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print(d)
+
+# creating dictionary using dict() constructor
+d1 = dict({1: 'Geeks', 2: 'For', 3: 'Geeks'})
+print(d1)
